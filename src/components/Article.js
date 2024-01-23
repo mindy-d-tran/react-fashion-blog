8a25e5c649b14398be2c36298b5b28871f2cd1b5
Mindy D Tran <mindytran98@gmail.com>
2024-01-23 13:21:49 -0500

    change img to blogimg style component and change img src because it won't load

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,3 +1,4 @@
+import { BlogImg } from "../styled";
 
 function Article() {
   return (
@@ -5,13 +6,13 @@ function Article() {
       <article>
         <time dateTime="2020-11-12">11/12/20</time>
         <h2>On The Street in Brooklyn</h2>
-        <img src="../img/blog-image-1.jpg" alt="blog img 1"></img>
+        <BlogImg src="https://ps-react-curriculum.herokuapp.com/fashionblog/blog-image-1.jpg" alt="blog img 1"></BlogImg>
         <p>lorem ipsum</p>
       </article>
       <article>
         <time dateTime="2020-11-11">11/11/20</time>
         <h2>Vintage in Vogue</h2>
-        <img src="../img/blog-image-2.jpg" alt="blog img 2"></img>
+        <BlogImg src="https://ps-react-curriculum.herokuapp.com/fashionblog/blog-image-2.jpg" alt="blog img 2"></BlogImg>
         <p>lorem ipsum</p>
       </article>
     </section>
