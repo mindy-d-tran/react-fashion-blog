68dfb2fee876fb80d1ae6246c46c969d4aa77798
Mindy D Tran <mindytran98@gmail.com>
2024-01-23 12:20:03 -0500

    hard code posts for aticle component

---
+++ src/components/Article.js
@@ -0,0 +1,21 @@
+
+function Article() {
+  return (
+    <section>
+      <article>
+        <time dateTime="2020-11-12">11/12/20</time>
+        <h2>On The Street in Brooklyn</h2>
+        <img src="../img/blog-image-1.jpg" alt="blog img 1"></img>
+        <p>lorem ipsum</p>
+      </article>
+      <article>
+        <time dateTime="2020-11-11">11/11/20</time>
+        <h2>Vintage in Vogue</h2>
+        <img src="../img/blog-image-2.jpg" alt="blog img 2"></img>
+        <p>lorem ipsum</p>
+      </article>
+    </section>
+  );
+}
+
+export default Article;
