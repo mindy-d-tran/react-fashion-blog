be75e864864eb5d8a3cea88869a7d012d4227dc5
Mindy D Tran <mindytran98@gmail.com>
2024-01-23 11:31:44 -0500

    create basic nav component

---
+++ src/components/Nav.js
@@ -0,0 +1,28 @@
+function Nav() {
+  return (
+    <>
+      <ul>
+        <li>
+          <a href="#">Women's</a>
+        </li>
+        <li>
+          <a href="#">Men's</a>
+        </li>
+        <li>
+          <a href="#">On the Street</a>
+        </li>
+        <li>
+          <a href="#">The Catwalk</a>
+        </li>
+        <li>
+          <a href="#">AdWatch</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+      </ul>
+    </>
+  );
+}
+
+export default Nav;
