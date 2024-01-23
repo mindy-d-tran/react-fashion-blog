ca8d83fa95590e11ca17c023d0594990df2437db
Mindy D Tran <mindytran98@gmail.com>
2024-01-23 11:45:33 -0500

    create basic footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import Nav from "./Nav";
+
+function Footer(){
+    return (
+        <footer>
+            <Nav/>
+            <p>2013 Valet Industries, Inc</p>
+        </footer>
+    )
+}
+
+export default Footer;
