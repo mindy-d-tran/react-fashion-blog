59a5d48874b94cac876e00804f8bf0108882a8ff
Mindy D Tran <mindytran98@gmail.com>
2024-01-23 11:23:48 -0500

    creating basic header component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header>
+      <h1>Sartre's List</h1>
+      <p className="subTitle">Better-Dressed People</p>
+
+    </header>
+  );
+}
+
+export default Header;
