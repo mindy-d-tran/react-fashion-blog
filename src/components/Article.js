31ce665cdc91147699ee51bb31dccb507ae140eb
Mindy D Tran <mindytran98@gmail.com>
2024-01-23 13:50:27 -0500

    change h2 to blogtitle styled component

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,17 +1,17 @@
-import { BlogImg } from "../styled";
+import { BlogImg, BlogTitle } from "../styled";
 
 function Article() {
   return (
     <main>
       <article>
         <time dateTime="2020-11-12">11/12/20</time>
-        <h2>On The Street in Brooklyn</h2>
+        <BlogTitle>On The Street in Brooklyn</BlogTitle>
         <BlogImg src="https://ps-react-curriculum.herokuapp.com/fashionblog/blog-image-1.jpg" alt="blog img 1"></BlogImg>
         <p>lorem ipsum</p>
       </article>
       <article>
         <time dateTime="2020-11-11">11/11/20</time>
-        <h2>Vintage in Vogue</h2>
+        <BlogTitle>Vintage in Vogue</BlogTitle>
         <BlogImg src="https://ps-react-curriculum.herokuapp.com/fashionblog/blog-image-2.jpg" alt="blog img 2"></BlogImg>
         <p>lorem ipsum</p>
       </article>
