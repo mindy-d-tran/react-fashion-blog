0ed33e6bd568c3f888d759abcc3728d35bea1c9b
Mindy D Tran <mindytran98@gmail.com>
2024-01-23 14:11:44 -0500

    add blog text and made the text longer

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,4 +1,4 @@
-import { BlogImg, BlogTitle } from "../styled";
+import { BlogImg, BlogText, BlogTitle } from "../styled";
 
 function Article() {
   return (
@@ -6,14 +6,36 @@ function Article() {
       <article>
         <time dateTime="2020-11-12">11/12/20</time>
         <BlogTitle>On The Street in Brooklyn</BlogTitle>
-        <BlogImg src="https://ps-react-curriculum.herokuapp.com/fashionblog/blog-image-1.jpg" alt="blog img 1"></BlogImg>
-        <p>lorem ipsum</p>
+        <BlogImg
+          src="https://ps-react-curriculum.herokuapp.com/fashionblog/blog-image-1.jpg"
+          alt="blog img 1"
+        ></BlogImg>
+        <BlogText>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </BlogText>
       </article>
       <article>
         <time dateTime="2020-11-11">11/11/20</time>
         <BlogTitle>Vintage in Vogue</BlogTitle>
-        <BlogImg src="https://ps-react-curriculum.herokuapp.com/fashionblog/blog-image-2.jpg" alt="blog img 2"></BlogImg>
-        <p>lorem ipsum</p>
+        <BlogImg
+          src="https://ps-react-curriculum.herokuapp.com/fashionblog/blog-image-2.jpg"
+          alt="blog img 2"
+        ></BlogImg>
+        <BlogText>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </BlogText>
       </article>
     </main>
   );
