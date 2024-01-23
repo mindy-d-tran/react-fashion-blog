df0acbeb796f6a079c2eb9e524f74e22bfaf81d5
Mindy D Tran <mindytran98@gmail.com>
2024-01-23 13:22:29 -0500

    create style component blogimg

--- src/styled.js
+++ src/styled.js
@@ -3,6 +3,11 @@ import styled from 'styled-components'
 export const Title = styled.h1`
     color: tomato;
 `;
+
+export const BlogImg = styled.img`
+    width: 100%
+`;
+
 export const CopyRight = styled.p`
     color: lightgray;
 `;
