022767c2198a9f4dabd1ae22afe972c29580fe4e
Mindy D Tran <mindytran98@gmail.com>
2024-01-23 11:36:24 -0500

    remove default and add header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
+import Nav from "./Nav";
+
 function Header() {
   return (
     <header>
       <h1>Sartre's List</h1>
       <p className="subTitle">Better-Dressed People</p>
-
+      <Nav />
     </header>
   );
 }
