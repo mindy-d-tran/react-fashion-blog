d3b53511b41cba37fc4ef74df99c18af084c1791
Mindy D Tran <mindytran98@gmail.com>
2024-01-23 14:07:31 -0500

    fix style for title component create blog text styled component

--- src/styled.js
+++ src/styled.js
@@ -1,18 +1,30 @@
 import styled from "styled-components";
 
 export const Title = styled.h1`
+  font-size: 4em;
+  margin-bottom: 0.1em;
   color: tomato;
 `;
 export const NavMenuUl = styled.ul`
-    list-style-type: none;
-    display: flex;
-    justify-content: space-between;
+  list-style-type: none;
+  display: flex;
+  justify-content: space-between;
 `;
 export const BlogImg = styled.img`
   width: 100%;
 `;
 export const BlogTitle = styled.h2`
-    font-size: 2em;
+  font-size: 2em;
+`;
+//style for drop cap https://css-tricks.com/snippets/css/drop-caps/
+export const BlogText = styled.p`
+  &::first-letter {
+    font-size: 6em;
+    float: left;
+    line-height: 0.65;
+    margin: 0.1em 0.1em 0.2em 0;
+    color: lightgray;
+  }
 `;
 export const CopyRight = styled.p`
   color: lightgray;
